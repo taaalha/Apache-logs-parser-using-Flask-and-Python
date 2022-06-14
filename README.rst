6eaf821131cf9f6ca7353338774f1f8e567d57b6
Talha <talha.khalid@aalto.fi>
2022-06-14 17:09:53 +0300

    added README

---
+++ README.rst
@@ -0,0 +1,23 @@
+Apache logs parser: Simple web GUI/UI to extract logs | Made with Flask, Python3
+===================================================================
+
+This is a simple application that parses Apache log files through filters, and displays it on a UI. 
+
+A user can upload any .txt log file. The parser would **extract** all the available ip's from the file, count 
+the number of times that ip is repeated in the logs, and display that number alongside the ip on a simple UI. 
+
+A sample log file is included with the code. It is recommended to use that for testing purposes. 
+
+Usage
+----------
+1. Ensure ``pip`` and ``pipenv`` are installed.
+2. Clone repository.
+3. cd into repository.
+4. Activate virtualenv: ``pipenv shell``
+5. Download flask: ``pip install flask``
+6. Run ``python3 parser_app.py``
+7. Go to the displayed local host address to access the application 
+
+
+
+
--- parser_app.py
+++ parser_app.py
@@ -1,4 +1,4 @@
-import re
+import re                       #required to use regex
 from collections import Counter
 
 from flask import Flask
@@ -8,23 +8,23 @@ from flask import request
 app = Flask(__name__)
 
 def parse(data):
-    pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' #this extracts the ip's from a given file. You can test it out by going to https://regex101.com/
     ips = re.findall(pattern, data)
 
-    results = Counter(ips).most_common(10)
+    results = Counter(ips).most_common(10) #only returns the 10 most common ip's in the file.
     return results 
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        log = request.files['log_file'].read()
+        log = request.files['log_file'].read() #flask reads the file in bytes format. we need to convert it into utf8
         txt = str(log, 'utf8')
         result = parse(txt)
     
         ban = []
         for key, value in result:
-            if value > 10:
+            if value > 10:                  #if the ip count is more than 10, ban it. 
                 ban.append({'ip': key, 'counts': value})
        
         return render_template('index.html', ips=ban)      
